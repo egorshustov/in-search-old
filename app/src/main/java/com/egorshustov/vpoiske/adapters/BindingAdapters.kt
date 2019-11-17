ec8eb6e7bb9bb40f76bed1ce518f3e90a51e2253
Egor Shustov <shustov_egor@mail.ru>
2019-11-17 08:46:49 +0300

    Images loaded with Glide using BindingAdapter

--- app/src/main/java/com/egorshustov/vpoiske/adapters/BindingAdapters.kt
+++ app/src/main/java/com/egorshustov/vpoiske/adapters/BindingAdapters.kt
@@ -1,10 +1,23 @@
 package com.egorshustov.vpoiske.adapters
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.egorshustov.vpoiske.data.User
 
 @BindingAdapter("app:items")
 fun setItems(listView: RecyclerView, userList: List<User>?) {
     (listView.adapter as UsersAdapter).submitList(userList)
+}
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+    }
 }
