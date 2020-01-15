442d95fcaeb637683c5fd6204b6342e08ac575cb
Egor Shustov <egorshustov.93@gmail.com>
2020-01-15 20:30:34 +0300

    menu item added - user list span count can be changed now

--- app/src/main/java/com/egorshustov/vpoiske/main/UserListFragment.kt
+++ app/src/main/java/com/egorshustov/vpoiske/main/UserListFragment.kt
@@ -3,8 +3,13 @@ package com.egorshustov.vpoiske.main
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.egorshustov.vpoiske.R
 import com.egorshustov.vpoiske.adapters.UsersAdapter
 import com.egorshustov.vpoiske.base.BaseFragment
@@ -17,14 +22,23 @@ class UserListFragment : BaseFragment<MainState, MainViewModel, FragmentUserList
 
     override val viewModel by activityViewModels<MainViewModel> { viewModelFactory }
 
+    private lateinit var gridLayoutManager: GridLayoutManager
+    private var spanCount = DEFAULT_SPAN_COUNT
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         setupListAdapter()
         observeOpenUserEvent()
     }
 
     private fun setupListAdapter() {
-        binding.recyclerUsers.adapter = UsersAdapter(viewModel)
+        gridLayoutManager =
+            GridLayoutManager(requireContext(), spanCount, RecyclerView.VERTICAL, false)
+        binding.recyclerUsers.apply {
+            layoutManager = gridLayoutManager
+            adapter = UsersAdapter(viewModel)
+        }
     }
 
     private fun observeOpenUserEvent() {
@@ -43,4 +57,24 @@ class UserListFragment : BaseFragment<MainState, MainViewModel, FragmentUserList
         startActivity(intent)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.user_list_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            R.id.item_change_view -> {
+                gridLayoutManager.apply {
+                    spanCount = (spanCount % MAX_SPAN_COUNT).inc()
+                    this@UserListFragment.spanCount = spanCount
+                }
+                true
+            }
+            else -> false
+        }
+
+    companion object {
+        private const val DEFAULT_SPAN_COUNT = 2
+        private const val MAX_SPAN_COUNT = 3
+    }
 }
