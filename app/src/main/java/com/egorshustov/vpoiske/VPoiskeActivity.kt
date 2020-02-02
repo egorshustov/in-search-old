155fca21d46f054eb2cd7f2dd51059942366efbd
Egor Shustov <egorshustov.93@gmail.com>
2020-02-03 01:17:06 +0300

    navigation and status bar are customized

--- app/src/main/java/com/egorshustov/vpoiske/VPoiskeActivity.kt
+++ app/src/main/java/com/egorshustov/vpoiske/VPoiskeActivity.kt
@@ -1,7 +1,10 @@
 package com.egorshustov.vpoiske
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -10,6 +13,8 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.egorshustov.vpoiske.userlist.UserListViewModel
+import com.egorshustov.vpoiske.util.AppTheme
+import com.google.android.material.appbar.AppBarLayout
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_v_poiske.*
 import javax.inject.Inject
@@ -29,6 +34,7 @@ class VPoiskeActivity : DaggerAppCompatActivity() {
         setContentView(R.layout.activity_v_poiske)
         setupNavigation()
         setChangeThemeListener()
+        customizeNavigationBar()
         observeSearchState()
     }
 
@@ -38,6 +44,10 @@ class VPoiskeActivity : DaggerAppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph, drawer_layout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         nav_view.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            findViewById<AppBarLayout>(R.id.layout_app_bar).fitsSystemWindows =
+                destination.id == R.id.userListFragment
+        }
     }
 
     private fun setChangeThemeListener() {
@@ -46,6 +56,31 @@ class VPoiskeActivity : DaggerAppCompatActivity() {
             viewModel.onNavChangeThemeClicked()
             recreate()
             true
+        }
+    }
+
+    private fun customizeNavigationBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val navigationBarColor = if (viewModel.currentTheme == AppTheme.DARK_THEME) {
+                R.color.colorMidnightExpress
+            } else {
+                customizeStatusBar()
+                R.color.colorAliceBlue
+            }
+            window.navigationBarColor = ContextCompat.getColor(this, navigationBarColor)
+        }
+    }
+
+    private fun customizeStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            findViewById<View>(android.R.id.content).systemUiVisibility =
+                findViewById<View>(android.R.id.content).systemUiVisibility or
+                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            findViewById<View>(android.R.id.content).systemUiVisibility =
+                findViewById<View>(android.R.id.content).systemUiVisibility or
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
 
